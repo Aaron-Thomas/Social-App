e7891817e777026eb91ca3b9603c7ea1e7a62cf9
Aaron Thomas <aaronthomas2000@hotmail.com>
2017-02-27 23:59:05 +0000

    Initial Firebase Integration

---
+++ SocialApp/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Aaron Thomas on 27/02/2017.
+//  Copyright © 2017 Aaron Thomas. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
