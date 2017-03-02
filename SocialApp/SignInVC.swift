5b0ece227de04525ab89cb227191bfc3e2dc6710
Aaron Thomas <aaronthomas2000@hotmail.com>
2017-03-02 22:52:09 +0000

    integrating FB login

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -14,6 +17,32 @@ class SignInVC: UIViewController {
         super.viewDidLoad()
     }
 
-
+    //integrating with Facebook
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("AARON: Unable to authenticate with Facebook- \(error)")
+            } else if result?.isCancelled == true {
+                print("AARON: User cancelled Facebook authentication")
+            } else {
+                print("AARON: Successfuly authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("AARON: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("AARON: Successfully authenticated with Firebase")
+            }
+})
+}
 }
 
